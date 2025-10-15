891320b99088e1959a0e75eb5198251148879855
Daruuu <thedaruny16@gmail.com>
2025-10-15 15:45:45 +0200

    add testing of serialization and deserealization of json file

--- src/main/java/com/daruuu/cashcard/CashCard.java
+++ src/main/java/com/daruuu/cashcard/CashCard.java
@@ -1,4 +1,11 @@
 package com.daruuu.cashcard;
 
+/*
+record  Name (String firstName, String lastName){
+}
+*/
+
+//record CashCard(Long id, Double amount, Name name) {
 record CashCard(Long id, Double amount) {
+
 }
--- src/test/java/com/daruuu/cashcard/CashCardJsonTest.java
+++ src/test/java/com/daruuu/cashcard/CashCardJsonTest.java
@@ -26,10 +26,30 @@ class CashCardJsonTest {
 
     @Test
     void cashCardSerializationTest() throws IOException {
+//        JacksonTester<CashCard> newJT = new JacksonTester<>();
+//        CashCard cashCard = new CashCard(99L, 123.45, new Name("Daruu", ""));
         CashCard cashCard = new CashCard(99L, 123.45);
+
         assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
-        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
-        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
-        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+//        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
+//        assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
+//        assertThat(json.write(cashCard)).hasJsonPathStringValue("@.name.lastName");
+
+//        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
+    }
+
+    @Test
+    void cashCardDeserializationTest() throws IOException {
+        String expected = """
+           {
+                   "id":99,
+               "amount":123.45
+           }
+           """;
+        assertThat(json.parse(expected))
+                .isEqualTo(new CashCard(99L, 123.45));
+//        assertThat(json.parseObject(expected).id()).isEqualTo(1000);
+//        assertThat(json.parseObject(expected).amount()).isEqualTo(67.89);
     }
 }
+
