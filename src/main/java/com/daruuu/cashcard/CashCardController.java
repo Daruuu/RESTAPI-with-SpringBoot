e2bad0b1cfaa62a9387bb09016aa1cc1a63e4370
Daruuu <thedaruny16@gmail.com>
2025-10-16 22:02:55 +0200

    add cashcardcontroller & update tdd

---
+++ src/main/java/com/daruuu/cashcard/CashCardController.java
@@ -0,0 +1,17 @@
+package com.daruuu.cashcard;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cashcard")
+class CashCardController {
+
+    //  add a handler method:
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<String> findById() {
+        return ResponseEntity.ok("{}");
+    }
+}
--- src/test/java/com/daruuu/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/daruuu/cashcard/CashCardApplicationTests.java
@@ -1,13 +1,46 @@
 package com.daruuu.cashcard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+//  this will start our springboot application and make it available for our TEST to perform requests to it.
+@SpringBootTest(webEnvironment =
+        SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    /*
+    - We've asked Spring to inject a test helper that will allow us to make HTTP
+        requests to the locally running application
+    - autowired: is a form of spring dependency injection, its best used only in tests.
+     */
+    @Autowired
+    TestRestTemplate restTemplate;
 
+/*
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcard/99", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+*/
+
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcard/99", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+        Number id = documentContext.read("$.id");
+        assertThat(id).isNotNull();
+    }
 }
